629ffd15a88fd37cbe0796f92c896a8c68da9c77
HariPalempati <HariKrishna@Swathis-MacBook-Pro.local>
2015-06-30 19:28:52 -0700

    Initial Commit

---
+++ FLCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FLCoreData
+//
+//  Created by Hari Krishna Palempati on 30/06/15.
+//  Copyright (c) 2015 Hari Krishna Palempati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FLCoreData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FLCoreData
+//
+//  Created by Hari Krishna Palempati on 30/06/15.
+//  Copyright (c) 2015 Hari Krishna Palempati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
